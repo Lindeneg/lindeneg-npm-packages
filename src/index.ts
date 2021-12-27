43d0dc2a8b12fefcbefcbc43ad567bc89eceeb5b
lindeneg <christian@lindeneg.org>
2021-12-27 17:29:20 +0100

    exports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as useSearch } from "./useSearch";
 export { default as useQueryParams } from "./useQueryParams";
 export { default as useHttp } from "./useHttp";
 export { default as useOnKey } from "./useOnKey";
+export { useMemoryCache, useBrowserCache } from "./useCache";
