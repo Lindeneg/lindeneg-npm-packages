ac3007150b6932f83f2fd713478167ae441f7055
lindeneg <christian@lindeneg.org>
2021-12-27 17:29:40 +0100

    allow event listenrs

--- src/useCache/cache.ts
+++ src/useCache/cache.ts
@@ -1,30 +1,38 @@
 import { EmptyObj } from "../shared";
 
-type CacheConfig = {
+export type CacheConfig<T extends EmptyObj> = {
   trim: number;
   ttl: number;
+  initial: Partial<T>;
 };
 
-type CacheEntry<T> = {
+export type CacheEntry<T> = {
   expires: number;
   value: T;
 };
 
-function getTime() {
+export type CacheData<T extends EmptyObj> = Partial<{
+  [K in keyof T]: CacheEntry<T[K]>;
+}>;
+
+export function getTime() {
   return new Date().getTime() / 1000;
 }
 
 export default class Cache<T extends EmptyObj> {
   private interval: NodeJS.Timer;
-  private config: CacheConfig;
-  private data: Partial<{ [K in keyof T]: CacheEntry<T[K]> }>;
+  private config: CacheConfig<T>;
+  private data: CacheData<T>;
+  private listeners: Partial<{ [K in keyof Cache<T>]: Cache<T>[K] }>;
 
-  constructor(config?: CacheConfig) {
+  constructor(config?: Partial<CacheConfig<T>>) {
     this.config = {
       trim: config?.trim || 600,
       ttl: config?.ttl || 3600,
+      initial: config?.initial || {},
     };
-    this.data = {};
+    this.listeners = {};
+    this.data = this.handleInitialData(this.config.initial);
 
     this.interval = setInterval(this.trim, this.config.trim * 1000);
   }
@@ -39,6 +47,10 @@ export default class Cache<T extends EmptyObj> {
     return keys;
   };
 
+  public size = () => {
+    return this.keys().length;
+  };
+
   public has = (key: keyof T) => {
     return typeof this.data[key] !== "undefined";
   };
@@ -47,15 +59,23 @@ export default class Cache<T extends EmptyObj> {
     return this.data[key] || null;
   };
 
+  public value = <K extends keyof T>(key: K): T[K] | null => {
+    const entry = this.data[key];
+    return entry ? entry.value : null;
+  };
+
   public set = <K extends keyof T>(key: K, value: T[K]) => {
+    this.listeners.set && this.listeners.set(key, value);
     this.data[key] = this.createEntry(value);
   };
 
   public remove = (key: keyof T) => {
+    this.listeners.remove && this.listeners.remove(key);
     delete this.data[key];
   };
 
   public clear = () => {
+    this.listeners.clear && this.listeners.clear();
     this.each((key) => {
       if (this.has(key)) {
         this.remove(key);
@@ -64,9 +84,28 @@ export default class Cache<T extends EmptyObj> {
   };
 
   public destruct = () => {
+    this.listeners.destruct && this.listeners.destruct();
     this.clear();
     clearInterval(this.interval);
   };
+
+  // TODO allow to listen for trim event
+  public on = <K extends "set" | "remove" | "clear" | "destruct">(
+    event: K,
+    callback: Cache<T>[K]
+  ): void => {
+    this.listeners[event] = callback;
+  };
+
+  private handleInitialData(obj?: Partial<T>): CacheData<T> {
+    if (obj) {
+      return Object.keys(obj).reduce(
+        (a, b) => ({ ...a, [b]: this.createEntry(obj[b]) }),
+        {}
+      );
+    }
+    return {};
+  }
 
   private each = (cb: (key: keyof T) => void) => {
     for (const key in this.data) {
