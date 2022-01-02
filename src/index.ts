8ceab6420c2833ed63c7693e6da4eb52253d3b88
lindeneg <christian@lindeneg.org>
2022-01-02 23:25:37 +0100

    remove invalid export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export { default as useSearch } from "./useSearch";
 export { default as useQueryParams } from "./useQueryParams";
-export { default as useHttp } from "./useHttp";
 export { default as useOnKey } from "./useOnKey";
 export { default as useMemoryCache } from "./useMemoryCache";
 export { default as useBrowserCache } from "./useBrowserCache";
