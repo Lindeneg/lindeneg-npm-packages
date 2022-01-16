15d54f137d185dc453bf445541dcf7eed39e4a4d
lindeneg <christian@lindeneg.org>
2022-01-16 14:36:24 +0100

    add rollup and ts

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-undef */
+import typescript from "rollup-plugin-typescript2";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import cleaner from "rollup-plugin-cleaner";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+
+const PACKAGE_ROOT_PATH = process.cwd();
+const { LERNA_ROOT_PATH } = process.env;
+
+export default {
+  input: PACKAGE_ROOT_PATH + "/index.ts",
+  output: [
+    {
+      file: "dist/bundle.cjs.js",
+      format: "cjs",
+    },
+    {
+      file: "dist/bundle.esm.js",
+      format: "esm",
+    },
+  ],
+  plugins: [
+    cleaner({
+      targets: ["./dist"],
+    }),
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: LERNA_ROOT_PATH + "/tsconfig-prod.json" }),
+    terser(),
+  ],
+};
