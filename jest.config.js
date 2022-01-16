245ab307936ca4db5fb1ff648a0caca21f51149a
lindeneg <christian@lindeneg.org>
2022-01-16 14:01:39 +0100

    remove jest config for now

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  roots: ["<rootDir>"],
-  testEnvironment: "jsdom",
-  testMatch: ["**/__tests__/**/*.test.+(ts|tsx|js)"],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
-};
