0cd2d31daaff7384e84ebc0f7e77edfdf4e715e5
lindeneg <christian@lindeneg.org>
2022-01-19 22:57:57 +0100

    dont listen to repeat events by default

--- packages/on-key/index.ts
+++ packages/on-key/index.ts
@@ -3,18 +3,22 @@ import { useEffect } from "react";
 export default function useOnKey(
   key: string,
   callback: (event: KeyboardEvent) => void,
-  type: "keydown" | "keyup" | "keypress" = "keydown"
+  type: "keydown" | "keyup" | "pressed" = "keydown"
 ) {
   useEffect(() => {
     if (typeof window !== "undefined") {
+      const e = type === "pressed" ? "keydown" : type;
       const onKeyDown = (event: KeyboardEvent) => {
         if (event.key === key) {
+          if (event.repeat && type !== "pressed") {
+            return;
+          }
           callback(event);
         }
       };
-      window.addEventListener(type, onKeyDown);
+      window.addEventListener(e, onKeyDown);
       return () => {
-        window.removeEventListener(type, onKeyDown);
+        window.removeEventListener(e, onKeyDown);
       };
     }
   }, [callback, key, type]);
