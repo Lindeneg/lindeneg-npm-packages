bbab29b35e63616fc12fddea3fe9330a19d9637c
lindeneg <christian@lindeneg.org>
2022-01-19 22:01:19 +0100

    update types tests

---
+++ packages/types/test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+code=0
+
+run_cleanup() {
+  echo 'cleaning ts-tests'
+  rm __tests__/types.ts-test.js
+  rm index.js
+}
+
+
+run_tests() {
+  echo 'running ts-tests'
+  cmd="tsc __tests__/types.ts-test.ts"
+  $cmd
+  if [[ $? -gt 0 ]]
+    then
+      code=1
+  fi
+}
+
+run_tests
+run_cleanup
+exit $code
