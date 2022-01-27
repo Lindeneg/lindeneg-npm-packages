f46c648a7cd7537078fd0fed70fbb577aa68fd97
lindeneg <christian@lindeneg.org>
2022-01-27 23:15:01 +0100

    memory-cache: male use of SafeOmit

--- packages/browser-cache/src/index.ts
+++ packages/browser-cache/src/index.ts
@@ -1,19 +1,20 @@
 import { useRef, useEffect } from 'react';
 import LS from '@lindeneg/ls-cache';
 import type { Config } from '@lindeneg/ls-cache';
-import type { EmptyObj } from '@lindeneg/types';
+import type { EmptyObj, SafeOmit } from '@lindeneg/types';
 
 export default function useBrowserCache<T extends EmptyObj>(
-  config?: Config<T>
+  config?: SafeOmit<Config<T>, 'delayInit'>
 ) {
-  const ref = useRef<LS<T>>(new LS(config));
+  const cacheRef = useRef<LS<T>>(new LS(config));
 
   useEffect(() => {
     return () => {
-      const { current } = ref;
+      const { current } = cacheRef;
+      // removes trim interval
       current.destruct();
     };
   }, []);
 
-  return { cache: ref.current };
+  return { cache: cacheRef.current };
 }
