f1d7ffa9b9a0714bb557e38bfbfc143bda3e1334
lindeneg <christian@lindeneg.org>
2022-01-29 23:57:14 +0100

    ls-cache: use SafeOmit

--- packages/ls-cache/src/types.ts
+++ packages/ls-cache/src/types.ts
@@ -1,8 +1,8 @@
-import type { EmptyObj } from '@lindeneg/types';
+import type { EmptyObj, SafeOmit } from '@lindeneg/types';
 import type { CacheConfig } from '@lindeneg/cache';
 
 export type Config<T extends EmptyObj> = Partial<
-  Omit<CacheConfig<T>, 'data'>
+  SafeOmit<CacheConfig<T>, 'data'>
 > & {
   prefix?: string;
   delayInit?: boolean;
