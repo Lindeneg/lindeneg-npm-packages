ae98724be77dfe730096e83fff61ce8b995747bc
lindeneg <christian@lindeneg.org>
2022-02-12 21:00:25 +0100

    root: refactor build script

--- build.sh
+++ build.sh
@@ -10,31 +10,24 @@ SCOPES=()
 
 BUILD_MSG="BUILDING "
 
-if [[ "$#" -gt 1 ]]
-  then
-    BUILD_MSG+="$# PACKAGES: "
-elif [[ "$#" -eq 0 ]]
-  then
-    BUILD_MSG+="ALL PACKAGES: "
-  else
-    BUILD_MSG+="1 PACKAGE: "
-fi
-
 if [[ ! -n $1 ]]
   then
     s="$PREFIX*"
     SCOPES+=( $s )
     LERNA_SCOPE=$s
+    BUILD_MSG+="ALL PACKAGES: "
 elif [ "$#" -eq 1 ]
   then
     s="$PREFIX$1"
     SCOPES+=( $s )
     LERNA_SCOPE=$s
+    BUILD_MSG+="1 PACKAGE: "
 else
   for arg in "$@" 
     do
       SCOPES+=( "$PREFIX$arg" )
   done
+  BUILD_MSG+="$# PACKAGES: "
   LERNA_SCOPE="{$(echo ${SCOPES[*]}} | sed 's/\s/,/')"
 fi
 
@@ -42,4 +35,10 @@ BUILD_MSG+="${SCOPES[*]}"
 
 echo $BUILD_MSG
 
-lerna exec --scope "'$LERNA_SCOPE'" -- "$CMD"
+result="lerna exec --scope '$LERNA_SCOPE' -- $CMD"
+
+$result
+
+CODE=$?
+
+exit $CODE
