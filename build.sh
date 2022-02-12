0949f883c7865872373c3c397d3d28aae0a8d5f0
lindeneg <christian@lindeneg.org>
2022-02-12 20:37:16 +0100

    root: update shell script

--- build.sh
+++ build.sh
@@ -2,22 +2,44 @@
 
 CMD='yarn rollup -c ../../rollup.config.js && yarn tsc --project ./tsconfig-prod.json --declaration --emitDeclarationOnly --declarationDir ./dist && yarn custom'
 
+LERNA_SCOPE=''
+
 PREFIX='@lindeneg/'
 
 SCOPES=()
 
+BUILD_MSG="BUILDING "
+
+if [[ "$#" -gt 1 ]]
+  then
+    BUILD_MSG+="$# PACKAGES: "
+elif [[ "$#" -eq 0 ]]
+  then
+    BUILD_MSG+="ALL PACKAGES: "
+  else
+    BUILD_MSG+="1 PACKAGE: "
+fi
+
 if [[ ! -n $1 ]]
   then
-    SCOPES+=( "$PREFIX*" )
+    s="$PREFIX*"
+    SCOPES+=( $s )
+    LERNA_SCOPE=$s
+elif [ "$#" -eq 1 ]
+  then
+    s="$PREFIX$1"
+    SCOPES+=( $s )
+    LERNA_SCOPE=$s
 else
   for arg in "$@" 
     do
       SCOPES+=( "$PREFIX$arg" )
   done
+  LERNA_SCOPE="{$(echo ${SCOPES[*]}} | sed 's/\s/,/')"
 fi
 
-CLEANED_SCOPES="{$(echo ${SCOPES[*]}} | sed 's/\s/,/')"
+BUILD_MSG+="${SCOPES[*]}"
 
-echo "BUILDING: $CLEANED_SCOPES"
+echo $BUILD_MSG
 
-lerna exec --scope "'$CLEANED_SCOPES'" -- "$CMD" > /dev/null
+lerna exec --scope "'$LERNA_SCOPE'" -- "$CMD" > /dev/null
