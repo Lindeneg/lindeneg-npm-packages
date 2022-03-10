115d98e92047e5b4068978e00a98403199f139c9
lindeneg <christian@lindeneg.org>
2022-03-10 21:11:39 +0100

    types: update test script

--- packages/types/test.sh
+++ packages/types/test.sh
@@ -3,19 +3,20 @@
 code=0
 
 run_cleanup() {
-  echo 'cleaning ts-tests'
+  echo '[TEST:TS]: cleaning tests'
   rm __tests__/types.ts-test.js
   rm ./src/index.js
 }
 
 
 run_tests() {
-  echo 'running ts-tests'
+  echo '[TEST:TS]: running tests'
   cmd="tsc __tests__/types.ts-test.ts"
   $cmd
   code=$?
+  echo '[TEST:TS]: finished with status code' $code
+  run_cleanup
+  exit $code
 }
 
 run_tests
-run_cleanup
-exit $code
